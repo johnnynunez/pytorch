c02f7c2466536a28ce2dfc8701264000c2f04437
CaoE <e.cao@intel.com>
2025-06-16 00:10:39 -0400

    add an option for cpp_wrapper to compile entry and kernel separately

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -2443,53 +2443,91 @@ class CppCodeCache:
 
         _set_gpu_runtime_env()  # cpp_extension consults the env
 
-        # Note the distinction between the two booleans.  We do minimal optimization if
-        # the optimized_code argument is present at all, since that's how the user of
-        # this function opts in, but we do compilation and linking in one step if the
-        # optimized_code argument is empty (as a micro-optimization).
-        main_build_option = CppTorchDeviceOptions(
-            compile_only=bool(optimized_code),
-            min_optimize=optimized_code is not None,
-            **compile_command,
-        )
-        optimized_build_option = CppTorchDeviceOptions(
-            compile_only=True, **compile_command
-        )
+        # init build option and command line for the main and optimized code
+        main_build_option = None
+        optimized_build_option = None
+        main_cmd_line = None
+        optimized_cmd_line = None
+        main_path = None
+        optimized_path = None
+        # init build option and command line for the single compilation path
+        input_path = None
+        cpp_build_option = None
+        vec_isa_cmd = None
 
-        def get_hashable_command_line(build_option: BuildOptionsBase) -> str:
-            """Writing the code to file will calculate a hash, which we need to vary if
-            the command line flags change.  This implements a mostly-generic way of
-            validating that."""
-            return CppBuilder(
-                name="o", sources="i", BuildOption=build_option
-            ).get_command_line()
+        if config.cpp_wrapper_build_separate:
+            # split compilation path - separate main code from optimized code
 
-        main_cmd_line = get_hashable_command_line(main_build_option)
-        optimized_cmd_line = get_hashable_command_line(optimized_build_option)
-
-        key, main_path = write(
-            main_code, "main.cpp", extra=f"{optimized_code} {main_cmd_line}"
-        )
-
-        # Don't bother writing if the argument is empty.
-        if optimized_code:
-            _, optimized_path = write(
-                optimized_code, "optimized.cpp", extra=optimized_cmd_line
+            # Note the distinction between the two booleans.  We do minimal optimization if
+            # the optimized_code argument is present at all, since that's how the user of
+            # this function opts in, but we do compilation and linking in one step if the
+            # optimized_code argument is empty (as a micro-optimization).
+            main_build_option = CppTorchDeviceOptions(
+                compile_only=bool(optimized_code),
+                min_optimize=optimized_code is not None,
+                **compile_command,
             )
+            optimized_build_option = CppTorchDeviceOptions(
+                compile_only=True, **compile_command
+            )
+
+            def get_hashable_command_line(build_option: BuildOptionsBase) -> str:
+                """Writing the code to file will calculate a hash, which we need to vary if
+                the command line flags change.  This implements a mostly-generic way of
+                validating that."""
+                return CppBuilder(
+                    name="o", sources="i", BuildOption=build_option
+                ).get_command_line()
+
+            main_cmd_line = get_hashable_command_line(main_build_option)
+            optimized_cmd_line = get_hashable_command_line(optimized_build_option)
+
+            key, main_path = write(
+                main_code, "main.cpp", extra=f"{optimized_code} {main_cmd_line}"
+            )
+
+            # Don't bother writing if the argument is empty.
+            if optimized_code:
+                _, optimized_path = write(
+                    optimized_code, "optimized.cpp", extra=optimized_cmd_line
+                )
+            else:
+                # Unused, but makes type checkers happy.
+                optimized_path = os.devnull
+
         else:
-            # Unused, but makes type checkers happy.
-            optimized_path = os.devnull
+            # Single compilation path - combine main and optimized code
+            cpp_build_option = CppTorchDeviceOptions(**compile_command)
+            command_gen = CppBuilder(
+                name="o", sources="i", BuildOption=cpp_build_option
+            )
+            # write function will calc source_code hash, the same source code with different
+            # ISA level should be generate different hash.
+            # So we need get a command_line which contains isa related parameter as a part of hash key.
+            # And then pass the command_line to below write function as extra parameter to
+            # guarantee the source code hash contains ISA difference.
+            vec_isa_cmd = repr(command_gen.get_command_line())
+            key, input_path = write(
+                (optimized_code or "") + main_code, "cpp", extra=vec_isa_cmd
+            )
 
-        if key not in cls.cache:
-            from torch.utils._filelock import FileLock
+        # Return early if we already have this in the cache
+        if key in cls.cache:
+            return cls.cache[key]
 
-            lock_path = os.path.join(get_lock_dir(), key + ".lock")
-            future: Optional[Future[Any]] = None
-            lib = None
+        from torch.utils._filelock import FileLock
 
+        lock_path = os.path.join(get_lock_dir(), key + ".lock")
+        future: Optional[Future[Any]] = None
+        lib = None
+
+        if config.cpp_wrapper_build_separate:
+            assert main_path is not None
+            assert main_build_option is not None
             # if requested, pre-compile any headers
             if config.cpp_cache_precompile_headers and not _IS_WINDOWS:
                 if header := cls._get_uncompiled_header(device_type):
+                    assert main_build_option is not None
                     main_build_option.precompiled_header = _precompile_header(
                         header,
                         main_cmd_line,
@@ -2501,6 +2539,7 @@ class CppCodeCache:
                 # so go ahead and precompile the relevant header here.  Revisit this
                 # decision if that ever changes.
                 if optimized_code and (header := _get_cpp_prefix_header(device_type)):
+                    assert optimized_build_option is not None
                     optimized_build_option.precompiled_header = _precompile_header(
                         header,
                         optimized_cmd_line,
@@ -2516,6 +2555,9 @@ class CppCodeCache:
             )
 
             if optimized_code:
+                assert optimized_path is not None
+                assert optimized_build_option is not None
+
                 optimized_name, _ = get_name_and_dir_from_output_file_path(
                     optimized_path
                 )
@@ -2549,25 +2591,53 @@ class CppCodeCache:
                 binary_path = normalize_path_separator(
                     main_builder.get_target_file_path()
                 )
+        else:
+            assert input_path is not None
+            assert cpp_build_option is not None
+            assert vec_isa_cmd is not None
 
-            def load_fn() -> Any:
-                nonlocal lib
-                if lib is None:
-                    if future is not None:
-                        future.result()
-                    result = worker_fn()
-                    assert result is None
-                    lib = cls._load_library(binary_path, key)
-                    assert lib is not None
-                return lib
+            output_name, output_dir = get_name_and_dir_from_output_file_path(input_path)
 
-            if submit_fn is not None:
-                with FileLock(lock_path, timeout=LOCK_TIMEOUT):
-                    if not os.path.exists(binary_path):
-                        future = submit_fn(worker_fn)
+            # if requested, pre-compile any headers
+            if (
+                config.cpp_cache_precompile_headers
+                and not _IS_WINDOWS
+                and (header_file := cls._get_uncompiled_header(device_type))
+            ):
+                cpp_build_option.precompiled_header = _precompile_header(
+                    header_file,
+                    vec_isa_cmd,
+                    **compile_command,
+                )
 
-            cls.cache[key] = load_fn
+            cpp_builder = CppBuilder(
+                name=output_name,
+                sources=input_path,
+                output_dir=output_dir,
+                BuildOption=cpp_build_option,
+            )
+            worker_fn = functools.partial(
+                _worker_compile_cpp,
+                lock_path,
+                (cpp_builder,),
+            )
+            binary_path = normalize_path_separator(cpp_builder.get_target_file_path())
 
+        def load_fn() -> Any:
+            nonlocal lib
+            if lib is None:
+                if future is not None:
+                    future.result()
+                worker_fn()
+                lib = cls._load_library(binary_path, key)
+            return lib
+
+        if submit_fn is not None:
+            with FileLock(lock_path, timeout=LOCK_TIMEOUT):
+                if not os.path.exists(binary_path):
+                    future = submit_fn(worker_fn)
+
+        cls.cache[key] = load_fn
         return cls.cache[key]
 
     @classmethod
@@ -2735,6 +2805,8 @@ class CppPythonBindingsCodeCache(CppCodeCache):
 
         Returns:
             A python version of ENTRY_FUNCTION()
+
+        Note: main_code and kernel_code are compiled separately only when cpp_wrapper_build_separate is True
         """
         parseargs = ", ".join(
             f"parse_arg<{argtype.replace('const ', '')}>(args, {n})"
